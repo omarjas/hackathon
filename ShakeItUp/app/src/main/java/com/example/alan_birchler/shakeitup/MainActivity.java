7357cb273cfecfd5c3676ec024978101e284c2d8
Alan Birchler De Allende <ab33068@vt.edu>
2017-02-18 16:03:37 -0500

    Made stuff prettier

--- ShakeItUp/app/src/main/java/com/example/alan_birchler/shakeitup/MainActivity.java
+++ ShakeItUp/app/src/main/java/com/example/alan_birchler/shakeitup/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.alan_birchler.shakeitup;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "Pacifico.ttf");
+        TextView deetsHours = (TextView)findViewById(R.id.deetshours);
+        TextView weekDays = (TextView)findViewById(R.id.weekdays);
+        TextView weekDaysTime = (TextView)findViewById(R.id.weekdaystime);
+        TextView weekends = (TextView)findViewById(R.id.weekends);
+        TextView weekendsTime = (TextView)findViewById(R.id.weekendstime);
+
+        deetsHours.setTypeface(pacificoFont);
+        weekDays.setTypeface(pacificoFont);
+        weekDaysTime.setTypeface(pacificoFont);
+        weekends.setTypeface(pacificoFont);
+        weekendsTime.setTypeface(pacificoFont);
     }
 }
