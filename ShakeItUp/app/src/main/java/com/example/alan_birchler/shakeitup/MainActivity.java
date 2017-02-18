e66d2bbc47690d137e18befa037bc85f2072f5d6
Alan Birchler De Allende <ab33068@vt.edu>
2017-02-18 15:19:10 -0500

    Edited front end to make thing purty

--- ShakeItUp/app/src/main/java/com/example/alan_birchler/shakeitup/MainActivity.java
+++ ShakeItUp/app/src/main/java/com/example/alan_birchler/shakeitup/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.alan_birchler.shakeitup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void shake(View view) {
+        Intent intent = new Intent(this, ShakeItActivity.class);
+        startActivity(intent);
+    }
+
+    public void menu(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
